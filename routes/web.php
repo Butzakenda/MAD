3fa8eb6759060ab39168e8ea346663cde22dd417
brostone45 <dilanale38@gmail.com>
2023-09-27 15:45:26 -0500

    Rutas para el Coach

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('/admin/formNewCoach');
+    return view('/welcome');
 });
 //Coach
 Route::get('index/coach', 'CoachController@index')->name('coach.index');
